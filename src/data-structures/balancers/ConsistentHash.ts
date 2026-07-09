ba3c460ea6ee9fe4206b4256ee8de4e6a35bbd5c
Doniyor <doniyor.otamurodov@gmail.com>
2026-07-09 15:31:20 +0500

    data-structures: Add nodes for consistent hashing implementation

--- src/data-structures/balancers/ConsistentHash.ts
+++ src/data-structures/balancers/ConsistentHash.ts
@@ -1,66 +1,66 @@
 import { createHash } from "crypto";
 
-interface Server {
+interface Node {
     hash: number;
     name: string;
 }
 
 class ConsistentHash {
-    servers: Server[] = []; // hash based sorted array
+    private nodes: Node[] = []; // hash based sorted array
+
+    constructor(private readonly virtualNodes: number) {}
 
     addServer(serverName: string): void {
-        const newServer: Server = { hash: this.hash(serverName), name: serverName };
+        const newNodes: Node[] = [];
 
-        if (this.servers.length === 0) {
-            this.servers.push(newServer);
+        for (let i = 0; i < this.virtualNodes; i++) {
+            const nodeHash = this.hash(`${serverName}#${i + 1}`);
 
-            return;
+            newNodes.push({ hash: nodeHash, name: serverName });
         }
 
-        if (this.servers.length === 1) {
-            if (newServer.hash > this.servers[0].hash) this.servers.push(newServer);
-            else this.servers.unshift(newServer);
+        newNodes.forEach((node) => {
+            const insertionIndex = this.getInsertionIndex(node.hash); // O(log(n))
 
-            return;
-        }
-
-        this.servers.splice(this.getInsertionIndex(newServer.hash), 0, newServer);
+            this.nodes.splice(insertionIndex, 0, node); // O(n)
+        }); // O(k * n) // Where k is number of nodes
     }
 
     removeServer(serverName: string): void {
-        this.servers = this.servers.filter((server) => server.name !== serverName);
+        this.nodes = this.nodes.filter((server) => server.name !== serverName);
     }
 
     getServer(key: string): string {
-        const insertionIndex = this.getInsertionIndex(this.hash(key)) % this.servers.length;
+        const insertionIndex = this.getInsertionIndex(this.hash(key)) % this.nodes.length;
 
-        return this.servers[insertionIndex].name;
+        return this.nodes[insertionIndex].name;
     }
 
-    hash(value: string): number {
+    private hash(value: string): number {
         //  0 <= value <= 2^32 - 1
         return createHash("sha256").update(value).digest().readUInt32BE(0);
     }
 
     private getInsertionIndex(hash: number): number {
+        if (this.nodes.length === 0) return 0;
+
         let l = 0,
-            r = this.servers.length - 1;
+            r = this.nodes.length - 1;
 
         while (l < r) {
             const m = Math.floor((l + r) / 2);
 
-            if (hash > this.servers[m].hash) l = m + 1;
+            if (hash > this.nodes[m].hash) l = m + 1;
             else r = m;
         }
 
-        return hash > this.servers[l].hash ? l + 1 : l;
+        return hash > this.nodes[l].hash ? l + 1 : l;
     }
 }
 
-const ch = new ConsistentHash();
+const ch = new ConsistentHash(100);
 "FDCEBA".split("").forEach((serverName) => ch.addServer(serverName));
 "KIOMSQ".split("").forEach((serverName) => ch.addServer(serverName));
-console.log(ch.getServer("apple"), ch.hash("apple"), ch.servers);
 // 0b00000000 => 0x00
 // 32byte => 16hex
 // 1byte = 8bit
